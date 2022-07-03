4125ae7c52f4296b91bcaaae3a0f3ddeb5aa61e9
MH4GF <h.miyagi.cnw@gmail.com>
2022-07-03 15:59:16 +0900

    setup prosemirror

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,16 @@
+import typescript from "@rollup/plugin-typescript";
+import nodeResolve from "rollup-plugin-node-resolve";
+
 export default {
-  input: 'index.ts',
+  input: "index.ts",
   output: {
-    file: 'dist/index.js',
-  }
+    file: "dist/index.js",
+  },
+  plugins: [
+    typescript(),
+    nodeResolve({
+      main: true,
+      browser: true,
+    }),
+  ],
 };
